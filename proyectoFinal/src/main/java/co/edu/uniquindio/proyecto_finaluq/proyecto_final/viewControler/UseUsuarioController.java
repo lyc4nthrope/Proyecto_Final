4f341a9e5e03bf254ca79bf9bcb94050ca0020d6
nosee8 <148650256+nosee8@users.noreply.github.com>
2024-03-31 02:00:56 -0500

    nada

--- proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewControler/UseUsuarioController.java
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewControler/UseUsuarioController.java
@@ -1,9 +1,11 @@
 package co.edu.uniquindio.proyecto_finaluq.proyecto_final.viewControler;
 
+import co.edu.uniquindio.proyecto_finaluq.proyecto_final.mapping.dto.ReservaDto;
 import co.edu.uniquindio.proyecto_finaluq.proyecto_final.mapping.dto.UsuarioDto;
 
 public class UseUsuarioController {
     UsuarioDto sesionUsuario;
+     ReservaDto reservaSeleccionada;
 
     public  void setSesionUsuario(UsuarioDto usuarioDto){this.sesionUsuario=usuarioDto;}
 
