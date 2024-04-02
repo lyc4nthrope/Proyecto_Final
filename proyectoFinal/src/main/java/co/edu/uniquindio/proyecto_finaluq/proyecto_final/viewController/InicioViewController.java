fd6d4c28d2ae994fbd0347a147dd83629f2c0d80
Cristhian Eduardo Osorio Restrepo <osoriocristhian86@gmail.com>
2024-04-02 18:24:48 -0500

    .

--- proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewController/InicioViewController.java
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewController/InicioViewController.java
@@ -38,4 +38,11 @@ public class InicioViewController {
         stage.show();
     }
 
+    public void switchRegistrar(ActionEvent event, UsuarioDto usuario) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("UsuarioRegistrarView.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
