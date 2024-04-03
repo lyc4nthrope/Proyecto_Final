e314d90dae8cd1b09474ccc809d37e53fe5d7679
Cristhian Eduardo Osorio Restrepo <osoriocristhian86@gmail.com>
2024-04-02 20:15:08 -0500

    .

--- proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewController/InicioViewController.java
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewController/InicioViewController.java
@@ -1,7 +1,6 @@
-package co.edu.uniquindio.proyecto_finaluq.proyecto_final.viewControler;
+package co.edu.uniquindio.proyecto_finaluq.proyecto_final.viewController;
 
 import co.edu.uniquindio.proyecto_finaluq.proyecto_final.controller.InicioController;
-import co.edu.uniquindio.proyecto_finaluq.proyecto_final.controller.UsuarioController;
 import co.edu.uniquindio.proyecto_finaluq.proyecto_final.mapping.dto.EmpleadoDto;
 import co.edu.uniquindio.proyecto_finaluq.proyecto_final.mapping.dto.UsuarioDto;
 import javafx.event.ActionEvent;
@@ -10,7 +9,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -58,6 +56,16 @@ public class InicioViewController {
                 switchEmpleadoUse(event, empleadoDtoAux);
             }
         }
+    }
+
+    @FXML
+    void onRegistrarseButtonAction(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("UsuarioRegistrarView.fxml"));
+        Parent root = loader.load();
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void salir(){
---
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/viewController/RegistrarViewController.java
@@ -0,0 +1,34 @@
+package co.edu.uniquindio.proyecto_finaluq.proyecto_final.viewController;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class RegistrarViewController {
+
+    @FXML
+    private Button btnAceptar;
+
+    @FXML
+    private Button btnCancelar;
+
+    @FXML
+    private TextField txtConfimarContraseña;
+
+    @FXML
+    private TextField txtContraseña;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    private TextField txtId;
+
+    @FXML
+    private TextField txtNombre;
+
+    
+
+
+}
+
