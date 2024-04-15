c566db16e928d51e599492aee128cab4385740cb
CamiloAlbertoOspinaAgudelo <148649416+CamiloAlbertoOspinaAgudelo@users.noreply.github.com>
2024-04-15 16:15:46 -0500

    dgfhj

--- proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/utils/ArchivoUtil.java
+++ proyectoFinal/src/main/java/co/edu/uniquindio/proyecto_finaluq/proyecto_final/utils/ArchivoUtil.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.proyecto_finaluq.proyecto_final.utils;
 import java.beans.XMLDecoder;
 import java.beans.XMLEncoder;
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -11,6 +12,30 @@ import java.util.logging.SimpleFormatter;
 
 public class ArchivoUtil {
     static String fechaSistema = "";
+
+    public static void guardarArchivo(String ruta,String contenido, Boolean flagAnexarContenido) throws IOException {
+
+        FileWriter fw = new FileWriter(ruta,flagAnexarContenido);
+        BufferedWriter bfw = new BufferedWriter(fw);
+        bfw.write(contenido);
+        bfw.close();
+        fw.close();
+    }
+
+    public static ArrayList<String> leerArchivo(String ruta) throws IOException {
+
+        ArrayList<String>  contenido = new ArrayList<String>();
+        FileReader fr=new FileReader(ruta);
+        BufferedReader bfr=new BufferedReader(fr);
+        String linea="";
+        while((linea = bfr.readLine())!=null)
+        {
+            contenido.add(linea);
+        }
+        bfr.close();
+        fr.close();
+        return contenido;
+    }
 
     public static void guardarRegistroLog(String mensajeLog, int nivel, String accion, String rutaArchivo)
     {
